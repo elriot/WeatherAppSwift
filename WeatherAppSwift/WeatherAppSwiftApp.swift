c6a6060b3518094667e81172c36d43f64a693e3b
Soopin Kim <spkim8901@gmail.com>
2024-08-19 15:45:12 -0700

    Initial commit

---
+++ WeatherAppSwift/WeatherAppSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppSwiftApp.swift
+//  WeatherAppSwift
+//
+//  Created by SOOPIN KIM on 2024-08-19.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
