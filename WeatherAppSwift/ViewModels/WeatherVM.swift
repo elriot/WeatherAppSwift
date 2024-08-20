a69c42cdfac9a13ea6dbab329dd50a281f406c38
Soopin Kim <spkim8901@gmail.com>
2024-08-19 21:56:12 -0700

    Success Fetch Data Called Api

--- WeatherAppSwift/ViewModels/WeatherVM.swift
+++ WeatherAppSwift/ViewModels/WeatherVM.swift
@@ -11,17 +11,22 @@ import SwiftUI
 final class WeatherVM: ObservableObject {
     @Published var weather: CurrentWeather = CurrentWeather.empty()
     @Published var type: WeatherType? = nil
+    @Published var weekly: WeeklyForecast? = nil
     
-    init(){
-        Api.shared.fetchWeather { weather in
-            guard let weather = weather else { return }
-            print("fetch data : \(weather)")
+    init() {
+        Api.shared.fetchWeather(lat: 49.2827, lon: -123.1216) { [weak self] weather, weekly in
+            guard let self = self, let weather, let weekly else { return }
             DispatchQueue.main.async {
                 self.setWeather(weather)
+                self.setWeekly(weekly)
+//                dump("weather : \(weather)")
+//                dump("weekly : \(weekly)")
             }
         }
     }
-    
+    private func setWeekly(_ weekly: WeeklyForecast) {
+        self.weekly = weekly
+    }
     private func setWeather(_ weather: CurrentWeather) {
         self.weather = weather
         self.type = getWeatherType(from: weather)
---
+++ WeatherAppSwift/Views/MiddleView/DailyWeatherView.swift
@@ -0,0 +1,18 @@
+//
+//  DailyWeatherView.swift
+//  WeatherAppSwift
+//
+//  Created by SOOPIN KIM on 2024-08-19.
+//
+
+import SwiftUI
+
+struct DailyWeatherView: View {
+    var body: some View {
+        Text("DailyWeatherView")
+    }
+}
+
+#Preview {
+    DailyWeatherView()
+}
