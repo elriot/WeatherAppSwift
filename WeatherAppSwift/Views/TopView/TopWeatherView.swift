7336193361d3646e3a1ae54fd3808db131e479b0
Soopin Kim <spkim8901@gmail.com>
2024-08-19 17:00:59 -0700

    TopViewImageDone

--- WeatherAppSwift/Views/TopView/TopWeatherView.swift
+++ WeatherAppSwift/Views/TopView/TopWeatherView.swift
@@ -15,12 +15,46 @@ struct TopWeatherView: View {
     var high: Int {
         Int(weather.main.temp_max)
     }
+    var type: WeatherType? {
+        if let description = weather.weather.first?.main {
+            print(WeatherType(description))
+            return WeatherType(description)
+        } else {
+            return nil
+        }
+    }
+    var icon: UIImage {
+        guard let type else { return UIImage() }
+        return type.icon ?? UIImage()
+    }
     
     var body: some View {
-        Text(weather.name)
-        Text("\(Int(weather.main.temp))")
-        Text(weather.weather.first?.description ?? "")
-        Text("Low: \(low)° High: \(high)°")
+        VStack {
+            ZStack {
+                Image(uiImage: icon)
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 100, height: 100)
+                    .foregroundColor(Color(type?.tint ?? .blue))
+            }
+            Text(weather.name)
+                .padding(0.5)
+            
+            Text("\(Int(weather.main.temp))°")
+                .padding(0.3)
+                .font(.title)
+                .fontWeight(.bold)
+            
+            Text(weather.weather.first?.description ?? "")
+                .padding(0.2)
+            
+            HStack {
+                Text("Low: \(low)°")
+                Text("|")
+                Text("High: \(high)°")
+            }
+            
+        }
     }
 }
 
